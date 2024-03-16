0353492be7f8fa9d5d445bd15bf93cb76b1556b4
Perforator <132618040+Yarik1357@users.noreply.github.com>
2024-03-16 12:21:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication
-
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QButtonGroup, QPushButton,QVBoxLayout,QHBoxLayout, QLineEdit, QLabel
+from PyQt5.QtCore import Qt  
 
 app = QApplication([])
-
 window = QWidget()
 window.resize(600, 500)
 window.setWindowTitle("Login")
 window.show()
 
 
+main_line = QVBoxLayout()
+line1 = QHBoxLayout()
+button1 = QPushButton("login")
+username = QLineEdit()
+password = QLineEdit()
+
+
+usernameLabel = QLabel("Username")
+passwordLabel = QLabel("Password")
+
+main_line.addWidget(usernameLabel, alignment=Qt.AlignCenter)
+
+
+
+window.setLayout(main_line)
+main_line.addWidget(username, alignment=Qt.AlignCenter)
+
+main_line.addWidget(passwordLabel, alignment=Qt.AlignCenter)
+
+main_line.addWidget(password, alignment=Qt.AlignCenter)
+main_line.addWidget(button1, alignment=Qt.AlignCenter)
+
+
+
+
+
+
 
 app.exec_()
-
